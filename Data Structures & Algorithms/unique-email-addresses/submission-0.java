7d189d0cf7de24e430121aae7a9a3724f6661bf3
Ariel del Rio <53499053+ArielDelRio@users.noreply.github.com>
2026-06-18 23:33:49 -0500

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+   public int numUniqueEmails(String[] emails) {
+        HashMap<String, Integer> simpleEmails = new HashMap<>();
+
+        for (String email : emails) {
+            String simpleEmail = getSimpleEmail(email);
+            simpleEmails.put(simpleEmail, simpleEmails.getOrDefault(simpleEmails, 0) + 1);
+        }
+        System.out.println(simpleEmails);
+        return simpleEmails.size();
+    }
+
+    public String getSimpleEmail(String email) {
+        StringBuilder simpleEmail = new StringBuilder();
+        boolean inDomain = false;
+
+        for (int i = 0; i < email.length(); i++) {
+            char current = email.charAt(i);
+
+            if (!inDomain && current == '.') {
+                continue;
+            }
+
+            if (current == '+') {
+                while (current != '@' && i < email.length()) {
+                    i++;
+                    current = email.charAt(i);
+                }
+            }
+
+            if (current == '@') {
+                inDomain = true;
+            }
+
+            simpleEmail.append(current);
+        }
+
+        return simpleEmail.toString();
+    }
+}
