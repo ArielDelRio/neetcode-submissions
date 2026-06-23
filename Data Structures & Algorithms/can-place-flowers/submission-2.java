a7eba9a3ecf372163d7bbd53619dfd275f6d79c5
Ariel del Rio <53499053+ArielDelRio@users.noreply.github.com>
2026-06-23 01:00:23 -0500

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+          for (int i = 0; i < flowerbed.length && n > 0; i++) {
+            boolean currentEmpty = flowerbed[i] == 0;
+            boolean leftEmpty = i == 0 || flowerbed[i - 1] == 0;
+            boolean rightEmpty = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
+
+            if (currentEmpty && leftEmpty && rightEmpty) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n == 0;
+    }
+}
