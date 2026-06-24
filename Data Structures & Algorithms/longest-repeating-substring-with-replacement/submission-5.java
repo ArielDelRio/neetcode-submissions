fc49938aab1faf3ccec735b45e12b296afef3fb9
Ariel del Rio <53499053+ArielDelRio@users.noreply.github.com>
2026-06-24 10:37:47 -0500

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.java
@@ -0,0 +1,61 @@
+class Solution {
+    // public int characterReplacement(String s, int k) {
+    //     int best = 0;
+    //     int left = 0;
+    //     int maxFreq = 0;
+    //     HashMap<Character, Integer> freq = new HashMap<>();
+
+    //     for (int right = 0; right < s.length(); right++) {
+    //         char rightChar = s.charAt(right);
+    //         freq.put(rightChar, freq.getOrDefault(rightChar, 0) + 1);
+    //         maxFreq = Math.max(maxFreq, freq.get(rightChar));
+    //         while ((right - left + 1) - maxFreq > k) {
+    //             char leftChar = s.charAt(left);
+    //             freq.put(leftChar, freq.get(leftChar) - 1);
+    //             left++;
+    //         }
+    //         best = Math.max(best, right - left + 1);
+    //     }
+    //     return best;
+    // }
+
+    public int characterReplacement(String s, int k) {
+        int best = 0;
+        int left = 0;
+        int maxFreq = 0;
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for(int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            int windowSize = right - left + 1;
+            
+            freq.put(rightChar, freq.getOrDefault(rightChar, 0) + 1);
+            maxFreq = getMaxFreq(freq);
+
+            while(!isValidWindow(windowSize, maxFreq, k)) {
+                char leftChar = s.charAt(left);
+                freq.put(leftChar, freq.get(leftChar) - 1);
+                left++;
+                windowSize = right - left + 1;
+            }
+
+            best = Math.max(best, windowSize);
+        }
+
+        return best;
+    }
+
+    private boolean isValidWindow(int windowSize, int maxFreq, int k) {
+        return windowSize - maxFreq <= k;
+    }
+
+    private int getMaxFreq(HashMap<Character, Integer> freq) {
+        int maxFreq = 0;
+        for (int count : freq.values()) {
+            maxFreq = Math.max(maxFreq, count);
+        }
+
+        return maxFreq;
+    }
+
+}
