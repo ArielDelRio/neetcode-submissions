ccaed572da161616892a227544b823289ab799d4
Ariel del Rio <53499053+ArielDelRio@users.noreply.github.com>
2026-06-27 13:29:28 -0500

    Add: is-palindrome - submission-17

---
+++ Data Structures & Algorithms/is-palindrome/submission-17.java
@@ -0,0 +1,33 @@
+class Solution {
+
+// Input: s = "tab a bat"
+// left = 0
+// right = 8
+
+            // System.out.println(right);
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+}
